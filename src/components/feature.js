417c6075bfe997920320ee0c67ef3f263623a957
Mike Farrow <contact@mikefarrow.co.uk>
2019-12-05 21:46:16 +0000

    testing images

--- src/components/feature.js
+++ src/components/feature.js
@@ -5,6 +5,7 @@ const Feature = (props) => (
   <SbEditable content={props.blok}>
     <div className="col-4">
       <h2>{props.blok.name}</h2>
+      {props.blok.icon && <img src={props.blok.icon} />}
     </div>
   </SbEditable>
 )
