4765b7829a091e79ab043a8b81140884809b176c
Mike Farrow <contact@mikefarrow.co.uk>
2019-12-05 21:31:06 +0000

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: 'mmPK4QQgw9k8YNu0D3gEiwtt',
+        homeSlug: 'home',
+        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft'
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
